129ddc059d647ea73fa16d75dc6e13e7fb19ba91
Michelle Clement <michellelclement@gmail.com>
2020-09-16 16:20:12 +0000

    Static files added. base template created. Nav bar inserted

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, flash, render_template, redirect, request, session, url_for
-from flask_pymongo import Pymongo
+from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
@@ -13,14 +13,14 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
 
-mongo = Pymongo(app)
+mongo = PyMongo(app)
 
 
 @app.route("/")
 @app.route("/get_recipes")
 def get_recipes():
     recipes = mongo.db.recipes.find()
-    return render_teample("recipes.html", recipes=recipes)
+    return render_template("recipes.html", recipes=recipes)
 
 
 if __name__ == "__main__":
