b95f427f52a4136ae781ed4ab89b77343366078c
Michelle Clement <michellelclement@gmail.com>
2020-09-17 09:41:56 +0000

    Sign up form complete

--- app.py
+++ app.py
@@ -29,6 +29,25 @@ def home():
 
 @app.route("/sign_up", methods=["GET", "POST"])
 def sign_up():
+    if request.method == "POST":
+        # Check username exists
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            return redirect(url_for("sign_up"))
+            flash("Username already exists")
+
+        sign_up = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(sign_up)
+
+        # Put user into a session cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        
     return render_template("sign_up.html")
 
 
