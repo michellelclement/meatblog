f21d8aa99174a9e46b0a5d56636de5a7b9e5367d
Michelle Clement <michellelclement@gmail.com>
2020-09-20 16:54:49 +0000

    added upload form button. JQuery scrips for Cloudinary to be fixed

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,6 @@ $(document).ready(function(){
     $('.collapsible').collapsible();
     $('select').formSelect();
   });
+
+$('.upload_form').append($.cloudinary.unsigned_upload_tag("meatblog", 
+  { cloud_name: 'du2vwqykf' }));
