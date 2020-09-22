cc5c226a3af202946e588e5b025755dbc9a8a811
Michelle Clement <michellelclement@gmail.com>
2020-09-22 13:54:00 +0000

    Add image via URL added to add recipe and working

--- app.py
+++ app.py
@@ -102,6 +102,7 @@ def add_recipe():
             "recipe_name": request.form.get("recipe_name"),
             "recipe_ingredients": request.form.get("recipe_ingredients"),
             "recipe_method": request.form.get("recipe_method"),
+            "image_url": request.form.get("image_url"),
             "created_by": session["user"]
         }
 
