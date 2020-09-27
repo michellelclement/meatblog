c8977e435c34df6520298069782da365274b0ccf
Michelle Clement <michellelclement@gmail.com>
2020-09-27 08:21:50 +0000

    Helper text added to sign up page

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, flash, render_template, redirect, request, session, url_for
+from flask import Flask, flash, render_template, \
+      redirect, request, session, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
