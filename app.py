57ff2e53256abe316daac02db0c6cba21a3372db
Michelle Clement <michellelclement@gmail.com>
2020-10-01 08:33:21 +0000

    Adding new testing doc

--- app.py
+++ app.py
@@ -29,7 +29,16 @@ def home():
 
 @app.route("/find_recipe")
 def find_recipe():
+    recipes = mongo.db.recipes.find()
+    
+    for recipe in recipes:
+        print(f"Unsorted recipes one by one: {recipe}")
+    
     recipes = mongo.db.recipes.find().sort("date_added")
+    
+    print("SORTED recipes one by one:")
+    for recipe in recipes:
+        print(recipe)
     return render_template("recipes.html", recipes=recipes)
 
 
